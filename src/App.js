9032cf8e28235c951fe2bc3e0384c5571f4815d1
Awodele Felix <felixrex82@gmail.com>
2024-04-12 12:24:31 +0100

    "version 1"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoLanding from "./pages/app-landing";
+import NewTask from "./pages/newTask";
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+     <Route index element={<TodoLanding/>} />
+     <Route path='/newtask' element={<NewTask/>} />
+    </>
+  )
+);
+
+
+
+
+const App = () => {
+    return <RouterProvider router={router} />;
+
 }
-
+    
+ 
 export default App;
